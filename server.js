c1b673d40d8d89e1a171379afddbd65c11b5a84f
romanfilov <romanfilov96@gmail.com>
2018-08-22 18:29:43 +0300

    "first commit"

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var app = express();
+//var path = require('path');
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+app.listen(4000);
