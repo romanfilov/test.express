2b63abb2da77fca9a6597e9cc83dd7b3f1f13c99
romanfilov <romanfilov96@gmail.com>
2018-08-28 18:19:49 +0300

    "add transform controls"

---
+++ public/js/main.js
@@ -0,0 +1,262 @@
+﻿// DOM ELEMENTS
+// var upload = document.createElement('input');
+// upload.setAttribute('type', 'file');
+// upload.setAttribute('id', 'upload');
+// document.body.appendChild(upload);
+// var update = document.createElement('button');
+// update.setAttribute('id', 'update');
+// document.body.appendChild(update);
+
+var mode = document.createElement('button');
+mode.flag = false;
+mode.value = 'scaling';
+mode.innerHTML = mode.value;
+mode.setAttribute('id', 'mode');
+document.body.appendChild(mode);
+
+mode.onclick = function(e) {
+    mode.flag ? mode.value = 'scaling' : mode.value = 'editing';
+    mode.innerHTML = mode.value;
+    mode.flag = !mode.flag;
+}
+
+// SOCKET
+var socket = io();
+
+
+
+
+/////// ADD AFTER CONFIG SAVING NEW MODEL
+
+// var uploader = new SocketIOFileUpload(socket);
+// uploader.listenOnInput(document.getElementById("upload"));
+// socket.on('onsaved', function(file) {
+//     loadModel(file.pathName);
+// });
+
+//////////////
+
+/////////// DELETE AFTER CONFIG
+
+var path = 'public/models/cube.obj';
+
+
+////////////////
+
+
+/// THREE JS
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 5);
+camera.up.set(0, 0, 1);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var controls = new THREE.OrbitControls( camera );
+
+
+var horPlaneGeometry = new THREE.PlaneBufferGeometry(6, 6, 10, 10);
+var horPlaneMaterial = new THREE.MeshLambertMaterial({color: 0x4553c1, transparent:true, opacity:.1, side: THREE.DoubleSide});
+var horPlane = new THREE.Mesh(horPlaneGeometry, horPlaneMaterial);
+
+scene.add(horPlane);
+
+// var verPlaneGeometry = new THREE.PlaneGeometry(6000, 6000, 10, 10);
+// var verPlaneMaterial = new THREE.MeshLambertMaterial({color: 0x454545, transparent: true, side: THREE.DoubleSide});
+// var verPlane = new THREE.Mesh(verPlaneGeometry, verPlaneMaterial);
+// verPlane.rotation.y = Math.PI / 2;
+
+//scene.add(verPlane);
+
+//var planes = [horPlane, verPlane];
+
+var axes = new THREE.AxesHelper( 5 );
+scene.add( axes  );
+
+
+
+
+
+
+var light = new THREE.AmbientLight( 0xffffff ); // soft white light
+scene.add( light );
+var loader = new THREE.OBJLoader();
+loadModel(path);
+var models = [];
+var point = null;
+var curModel;
+var obj;
+
+function loadModel(path) {
+    loader.load(
+        path,   
+        function (object) {
+           object.traverse( function( model ) {
+               if( model instanceof THREE.Mesh ) {
+       
+                   model.material.side = THREE.DoubleSide;
+                   model.material.color = new THREE.Color(0xcccccc);
+                   model.material.wireframe = false;
+                   var geometry = new THREE.Geometry ();
+                   geometry.fromBufferGeometry (model.geometry);
+                   geometry.mergeVertices();
+                   //geometry.computeVertexNormals ();
+                   models.push(model);
+                   model.geometry = geometry;
+               }
+               scene.add(object);
+           });
+        });
+}
+
+
+
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+var points = [];
+
+///////// MODELS OBJECT GEOMETRY -> CREATE POINT
+var geometry;
+var index;
+
+/// MODELS FOR TRANFORM CONTROLS
+var model;
+
+function onMouseClick(e) {
+
+    
+	mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (e.clientY / window.innerHeight) * 2 + 1;
+    //////////////////
+    ////////////// ROTATION MODE
+    ////////////////////
+    raycaster.setFromCamera(mouse, camera);
+    var modelIntersects = raycaster.intersectObjects(models);
+    console.log(modelIntersects);
+    if(modelIntersects.length > 0) {
+
+        model = modelIntersects[0].object;
+
+        
+
+        transformControl.attach(model);
+
+        scene.add(transformControl);
+    }
+
+    
+    ///////////////////////////////
+
+
+    /////////////////
+    ////////// EDITING MODE
+    //////////////////////
+
+    // var point = null;
+    // raycaster.setFromCamera(mouse, camera);
+    // if(geometry) {
+    //     geometry.boundingBox = null;
+    //     geometry.boundingSphere = null;
+    // }
+    // var modelIntersects = raycaster.intersectObjects(models);
+    // var pointIntersects = raycaster.intersectObjects(points);
+    // if(pointIntersects.length > 0) {
+    //     dragControls.addEventListener('drag', function() {
+    //         point = pointIntersects[0].object;
+    //         geometry.vertices[point.index].copy(point.position);
+    //         geometry.verticesNeedUpdate = true;
+    //     })
+    // } else if (points.length > 0 && pointIntersects.length === 0) {
+    //     scene.remove(scene.getObjectByName('point'));
+    //     points.pop();
+    // }  
+    
+    // if (points.length == 0 && modelIntersects.length > 0) {
+    //     point = getPoint(modelIntersects);
+    //     if(point) {
+    //         dragControls = new THREE.DragControls(points, camera, renderer.domElement);
+    //         dragControls.addEventListener('dragstart', function () {
+    //             controls.enabled = false;
+    //         });
+    //         dragControls.addEventListener('dragend', function () {
+    //             controls.enabled = true;
+    //         });
+    //         points.push(point);
+    //         scene.add(point);
+    //     }
+    // }
+
+    ///////////////////////////////////////////////////
+
+
+}
+
+
+///////////////
+///////////// TRANSFORM CONTROLS AND HANDLERS
+
+var transformControl = new THREE.TransformControls(camera, renderer.domElement);
+        
+transformControl.addEventListener('change', render);
+transformControl.addEventListener('mouseUp', transformMouseUp);
+transformControl.addEventListener('mouseDown', transformMouseDown);
+//transformControl.addEventListener('objectChange', transformObjectChanged);
+
+// function transformObjectChanged() {
+//     console.log('change');
+// }
+
+function transformMouseDown() {
+    
+}
+
+function transformMouseUp() {
+    controls.reset();
+}
+///////////////////////////
+/////////////////////////////////////
+
+/////////
+////// EDITING MODE CREATE POINT
+/////////////// 
+function getPoint(modelIntersects) {
+    
+    var face = modelIntersects[0].face.clone();
+    geometry = modelIntersects[0].object.geometry;
+    var pointCoords = modelIntersects[0].point;
+    var faceVertices = [];
+    geometry.vertices.forEach(function(item, i) {
+        if(i == face.a || i == face.b || i == face.c) {
+            var selItem = item.clone();
+            selItem.index = i;
+            faceVertices.push(selItem); 
+        }
+    })
+    faceVertices.forEach(function(item) {
+        item.distance = pointCoords.distanceTo(item);
+    })
+    faceVertices.sort(function(a, b) {
+        return a.distance - b.distance;
+    })
+    point = new THREE.Mesh( new THREE.SphereGeometry(0.1), new THREE.MeshBasicMaterial({color: 0x404040}));
+    point.name = 'point';
+    point.position.copy(faceVertices[0]);
+    point.index = faceVertices[0].index;
+    return point;
+
+}
+
+//////////////////////////////////////// END EDITING MODE
+
+function render() {
+    requestAnimationFrame(render);
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+renderer.domElement.addEventListener('mousedown', onMouseClick);
+
+render();
--- server.js
+++ server.js
@@ -1,7 +1,76 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-//var path = require('path');
-app.get('/', function(req, res) {
+var path = require('path');
+var http = require('http').createServer(app);
+var fs = require('fs-extra');
+//var cookieParser = require('cookie-parser');
+var siofu = require('socketio-file-upload');
+var io = require('socket.io').listen(http);
+// var session = require('express-session');
+// var fileUpload = require('express-fileupload');
+
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+  });
+
+
+
+app.use(express.static(path.join(__dirname)));
+app.use(express.static('node_modules/three'));
+app.use(express.static('node_modules')); 
+// app.use(session({
+//     secret: 'secretKey',
+//     key: 'sid',
+//     saveUninitialized: false,
+//     resave: true,
+//     cookie: {
+//         httpOnly: true,
+//         secure: true,
+//         maxAge: null
+//     }
+// }));
+app.use(siofu.router);
+
+
+app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
-app.listen(4000);
+
+
+
+
+io.on('connection', function(socket){
+
+
+    // ADD AFTER CONFIG SAVE NEW MODEL
+
+    // var path = './public/users/' + socket.id;
+    // fs.mkdirsSync(path + '/models');
+    // var uploader = new siofu();
+    // socket.on('disconnect', function(){
+    //     fs.removeSync(path);
+    // });
+    // var uploader = new siofu();
+    // uploader.dir = './public/users/'+ socket.id +'/models';
+    // uploader.listen(socket);
+
+    // uploader.on('saved', function(event){
+    //     socket.emit('onsaved', event.file);
+    // });
+    
+
+    // uploader.on("error", function(event){
+    //     console.log("Error from uploader", event);
+    // });
+
+    /////////////////////////////////////////
+
+
+
+});
+
+
+
+
+
